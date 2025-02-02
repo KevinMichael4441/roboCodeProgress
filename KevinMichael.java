34bd91d6e2c8d36ac03927af1d97c77d81182e99
Kevin Michael <c00313609@setu.ie>
2025-02-02 21:38:54 +0000

    TrackFire

--- KevinMichael.java
+++ KevinMichael.java
@@ -1,6 +1,7 @@
 package KM;
 import robocode.*;
 import java.awt.Color;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -23,10 +24,10 @@ public class KevinMichael extends Robot
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like	
+			turnGunLeft(360);
 			ahead(52);
 			turnGunRight(360);
 			back(52);
-			turnGunRight(360);
 			stayInBounds();
 		}
 	}
@@ -37,19 +38,32 @@ public class KevinMichael extends Robot
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Problem with this is losing in Semi-Finals :(
 		// We'll cross that bridge then
+
 		if (!e.isSentryRobot())
 		{
-			fire(4);
-		}	
-	}
+			double absoluteBearing = getHeading() + e.getBearing();
+			double gunBearing = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(15);
-		stayInBounds();
+			if (Math.abs(gunBearing) <= 3) 
+			{
+				turnGunRight(gunBearing);
+				// Calling fire() only if gun is cooled so that we don't waste a turn (calling fire() uses up a turn)
+				if (getGunHeat() == 0) 
+				{
+					fire(Math.min(3,getEnergy()-1));	//Fires if more than 1 energy 
+				}
+			} 
+			else
+			{
+				turnGunRight(gunBearing);
+			}
+
+			if (gunBearing == 0) //Makig sure opponent didn't move away by scanning again
+			{
+				scan();
+			}
+
+		}	
 	}
 	
 	public void stayInBounds() 
